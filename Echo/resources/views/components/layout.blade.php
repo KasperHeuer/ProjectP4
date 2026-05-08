0155ed33c8cafe7eff4546fd6547c5baf97c5c10
KasperHeuer <kasper100@ziggo.nl>
2026-05-08 12:08:19 +0200

    Tailwind toegevoegd

--- Echo/resources/views/components/layout.blade.php
+++ Echo/resources/views/components/layout.blade.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Document</title>
 </head>
 
-<body>
-<x-header/>
-{{ $slot }}
+<body class="">
+    <x-header />
+    {{ $slot }}
 </body>
 
 </html>
