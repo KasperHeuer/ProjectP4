8f9cad365499ded032350b0f21d42f99b7027c82
KasperHeuer <kasperheuer@ziggo.nl>
2026-05-08 14:40:53 +0200

    Kleine error fix

--- Echo/routes/web.php
+++ Echo/routes/web.php
@@ -3,17 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
-    return view('home');
+    return view('Home');
 });
 
 Route::get("/register", function () {
-    return view('register');
+    return view('Register');
 });
 
 Route::get("/login", function () {
-    return view('login');
+    return view('Login');
 });
 
 Route::get('/chat', function () {
-    return view('chat');
+    return view('Chat');
 });
